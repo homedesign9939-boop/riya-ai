68b9380e08c85ca0f6ee1aadf27f18697a0fcd7a
homedesign9939-boop <homedesign9939@gmail.com>
2026-09-09 23:32:35 +0530

    Update MainActivity.kt

--- app/src/main/java/com/example/riyaai/MainActivity.kt
+++ app/src/main/java/com/example/riyaai/MainActivity.kt
@@ -5,7 +5,6 @@ import android.os.Build
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import com.example.riyaai.R
 
 class MainActivity : AppCompatActivity() {
     private var isRunning = false
